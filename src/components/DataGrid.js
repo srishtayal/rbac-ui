ba66d127283794b135a2ced37dd6fa36a47aeb3e
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2024-11-26 21:35:52 +0530

    add status column

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -5,31 +5,30 @@ import Button from '@mui/material/Button';
 
 export default function DataGridDemo() {
   const [rows, setRows] = useState([
-    { id: 1, firstName: 'Jon', lastName: 'Snow', age: 14, role: 'User', permissions: ['Read'] },
-    { id: 2, firstName: 'Cersei', lastName: 'Lannister', age: 31, role: 'Admin', permissions: ['Read', 'Write', 'Delete'] },
-    { id: 3, firstName: 'Arya', lastName: 'Stark', age: 11, role: 'Editor', permissions: ['Read', 'Write'] },
-    { id: 4, firstName: 'Daenerys', lastName: 'Targaryen', age: 25, permissions: ['Read', 'Write', 'Delete'] },
+    { id: 1, firstName: 'Jon', lastName: 'Snow', age: 14, role: 'User', permissions: ['Read'], status:'Active' },
+    { id: 2, firstName: 'Cersei', lastName: 'Lannister', age: 31, role: 'Admin', permissions: ['Read', 'Write', 'Delete'], status:'Active' },
+    { id: 3, firstName: 'Arya', lastName: 'Stark', age: 11, role: 'Editor', permissions: ['Read', 'Write'], status:'Inctive' },
+    { id: 4, firstName: 'Daenerys', lastName: 'Targaryen', age: 25, permissions: ['Read', 'Write', 'Delete'], status:'Active' },
   { id: 5, firstName: 'Tyrion', lastName: 'Lannister', age: 35, permissions: ['Read', 'Write'] },
-  { id: 6, firstName: 'Sansa', lastName: 'Stark', age: 22, permissions: ['Read'] },
-  { id: 7, firstName: 'Bran', lastName: 'Stark', age: 17, permissions: ['Write'] },
-  { id: 8, firstName: 'Joffrey', lastName: 'Baratheon', age: 20, permissions: [] },
-  { id: 9, firstName: 'Robb', lastName: 'Stark', age: 25, permissions: ['Write'] },
-  { id: 10, firstName: 'Jaime', lastName: 'Lannister', age: 42, permissions: ['Delete'] },
-  { id: 11, firstName: 'Samwell', lastName: 'Tarly', age: 29, permissions: ['Read', 'Write', 'Delete'] },
-  { id: 12, firstName: 'Gendry', lastName: 'Baratheon', age: 24, permissions: ['Read'] },
-  { id: 13, firstName: 'Podrick', lastName: 'Payne', age: 21, permissions: ['Write', 'Read'] },
-  { id: 14, firstName: 'Varys', lastName: 'Targaryen', age: 45, permissions: ['Read', 'Write'] },
-  { id: 15, firstName: 'Missandei', lastName: 'Naath', age: 27, permissions: ['Read', 'Write'] },
-  { id: 16, firstName: 'Theon', lastName: 'Greyjoy', age: 30, permissions: ['Read', 'Write'] },
-  { id: 17, firstName: 'Yara', lastName: 'Greyjoy', age: 28, permissions: ['Delete'] },
-  { id: 18, firstName: 'Tormund', lastName: 'Giantsbane', age: 34, permissions: ['Read', 'Write'] },
-  { id: 19, firstName: 'Jorah', lastName: 'Mormont', age: 40, permissions: ['Read', 'Write'] },
-  { id: 20, firstName: 'Brienne', lastName: 'Tarth', age: 35, permissions: ['Write', 'Delete'] }
+  { id: 6, firstName: 'Sansa', lastName: 'Stark', age: 22, permissions: ['Read'], status:'Active' },
+  { id: 7, firstName: 'Bran', lastName: 'Stark', age: 17, permissions: ['Write'], status:'Inctive' },
+  { id: 8, firstName: 'Joffrey', lastName: 'Baratheon', age: 20, permissions: [], status:'Active' },
+  { id: 9, firstName: 'Robb', lastName: 'Stark', age: 25, permissions: ['Write'], status:'Active' },
+  { id: 10, firstName: 'Jaime', lastName: 'Lannister', age: 42, permissions: ['Delete'], status:'Active' },
+  { id: 11, firstName: 'Samwell', lastName: 'Tarly', age: 29, permissions: ['Read', 'Write', 'Delete'], status:'Active' },
+  { id: 12, firstName: 'Gendry', lastName: 'Baratheon', age: 24, permissions: ['Read'], status:'Active' },
+  { id: 13, firstName: 'Podrick', lastName: 'Payne', age: 21, permissions: ['Write', 'Read'], status:'Active' },
+  { id: 14, firstName: 'Varys', lastName: 'Targaryen', age: 45, permissions: ['Read', 'Write'], status:'Inctive' },
+  { id: 15, firstName: 'Missandei', lastName: 'Naath', age: 27, permissions: ['Read', 'Write'], status:'Active' },
+  { id: 16, firstName: 'Theon', lastName: 'Greyjoy', age: 30, permissions: ['Read', 'Write'], status:'Active' },
+  { id: 17, firstName: 'Yara', lastName: 'Greyjoy', age: 28, permissions: ['Delete'], status:'Active' },
+  { id: 18, firstName: 'Tormund', lastName: 'Giantsbane', age: 34, permissions: ['Read', 'Write'], status:'Inctive' },
+  { id: 19, firstName: 'Jorah', lastName: 'Mormont', age: 40, permissions: ['Read', 'Write'] , status:'Active' },
+  { id: 20, firstName: 'Brienne', lastName: 'Tarth', age: 35, permissions: ['Write', 'Delete'], status:'Active' }
   ]);
 
   const handleEdit = (id) => {
     alert(`Editing user with ID: ${id}`);
-    // Implement the logic to edit the user here.
   };
 
   const handleDelete = (id) => {
@@ -39,7 +38,7 @@ export default function DataGridDemo() {
   };
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'id', headerName: 'ID', width: 70 },
     {
       field: 'firstName',
       headerName: 'First Name',
@@ -56,22 +55,21 @@ export default function DataGridDemo() {
       field: 'age',
       headerName: 'Age',
       type: 'number',
-      width: 110,
+      width: 60,
       editable: true,
     },
     {
       field: 'role',
       headerName: 'Role',
-      width: 150,
+      width: 130,
       editable: true,
     },
     {
         field: 'permissions',
         headerName: 'Permissions',
-        width: 200,
+        width: 150,
         editable: true,
-      }
-      ,
+      },
     {
       field: 'actions',
       headerName: 'Actions',
@@ -99,6 +97,12 @@ export default function DataGridDemo() {
         </>
       ),
     },
+    {
+        field: 'status',
+        headerName: 'Status',
+        width: 200,
+        editable: true,
+      }
   ];
 
   return (
